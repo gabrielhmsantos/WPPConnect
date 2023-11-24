229c2f8f4ecfb67d439f45cd612f36d4a876b2fb
ShadowFox <fefisfefe@gmail.com>
2023-11-23 23:40:02 -0300

    :)

---
+++ wppconnect-nginx/conf.d/hosts.lua
@@ -0,0 +1,8 @@
+
+local hosts = {"172.20.0.2", "172.20.0.3"}
+local ports = {21465, 21465}
+
+local redis_server = "172.20.0.7"
+local redis_port = 6379
+
+return hosts, ports, redis_server, redis_port
